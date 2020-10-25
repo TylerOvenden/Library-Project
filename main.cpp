d6beb05c17c8c1ccfcf6b10f1209d80ababf8459
Avery Berry <arb232305@gmail.com>
2020-10-25 14:17:56 -0400

    Ignor vs and update main

--- main.cpp
+++ main.cpp
@@ -11,21 +11,57 @@ vector<Student> loadStudents() {
 	return students;
 }
 
+// Load list of books from book.txt
+vector<Book> loadBooks() {
+	vector<Book> books;
+	Book fakeBook = Book();
+	books.push_back(fakeBook);
+	return books;
+}
+
+// Prompt for user input
+string promptForInput(string description) {
+	cout << "Enter your " << description << ": ";
+	string input;
+	cin >> input;
+	return input;
+}
 
 int main() {
 
+	vector<Book> books = loadBooks();
+	vector<Student> students = loadStudents();
+
+	// Ask for username and password
+	string username = promptForInput("username");
+	string password = promptForInput("password");
 
 
+	bool authenticated = false;
+	Student authenticatedStudent;
 
+	// Compare username and password to all in file
+	for (int i = 0; i < students.size(); i++) {
+		Student student = students.at(i);
+		if ((student.getUser() == username) && 
+			(student.getPass() == password)){
+			authenticatedStudent = student;
+			authenticated = true;
+			break;
+		}
+	}
 
-
-
-
-
-
-
-
-
-
-
+	// Check if login was successful 
+	if (authenticated = false) {
+		cout << "The username or password was incorrect" << endl;
+		return 0;
+	}
+	else {
+		// Code after authentication success!
+		//
+		//
+		//
+		//
+		//
+	}
 }
