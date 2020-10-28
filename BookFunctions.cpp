f44b4c1134c9e1aa9062f7c9da83e703ddddc041
TylerOvenden <tovenden4386@bths.edu>
2020-10-28 15:22:00 -0400

    Update BookFunctions.cpp
    adding book

--- BookFunctions.cpp
+++ BookFunctions.cpp
@@ -8,13 +8,26 @@
 #include "BookFunctions.h"
 using namespace std;
 
-void BookFunctions::addCopy(Student s, Book b) {
+void BookFunctions::addCopy(Student s, Book b, vector<Book>& v) {
 	//need to implement time function rn just using 1 
 	//need to check if book is borrowed, too many books borrowed
 	//
-	string reader = s.getUser();
+	int currentAmount = s.getBook();
+	if (currentAmount == 5) {
+		cout << "too many books " << endl;
+	}
+	string reader = s.getUser();  
 	b.setRead(reader);
 	b.setStart(1);
 	b.setEnd(b.getStart() + 30);
-	 
+	v.push_back(b);
+}
+void BookFunctions::removeCopy(Student s, Book b, vector<Book>& v) {
+	for (int i = 0; i < v.size(); i++) {
+		
+
+	}
+	
+	b.setRead("");
+
 }
