4363337e2f5b1d63cc8faf6e55a585c08eba9ffb
TylerOvenden <tovenden4386@bths.edu>
2020-11-01 16:10:55 -0500

    Update Book.h

--- Book.h
+++ Book.h
@@ -1,45 +1,31 @@
 #pragma once
+#pragma once
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <list>
-#include "Student.h"
-#include <ctime>
+#include <vector>
+#include "Book.h"
+//#include "Student.h"
 using namespace std;
-class Book {
+class Student {
 private:
-	string title;
-	string author;
-	int isbn;
-	string category;
-	int id;
-	string reader;
-	double start;
-	double expiration;
-	bool borrow;
-	bool overdue;
-
-public:
-	Book();
-	Book(string title, string author, int isbn, string category, int id);
-	void setTitle(string t);
-	string getTitle();
-	void setAuthor(string aut);
-	string getAuthor();
-	void setISBN(int is);
-	int getISBN();
-	void setCat(string cat);
-	string getCat();
-	void setID(int ide);
-	int getID();
-	void setRead(string read);
-	string getRead();
-	void setStart();
-	double getStart();
-	void setEnd(double end);
-	double getEnd();
-	double getDiff();
-	void findOverdue(Student s, vector<Book>& borrow);
-	void addCopy(Student s, int id, vector<Book>& books, vector<Book>& borrow);
-	void removeCopy(Student s, int id, vector<Book>& books, vector<Book>& borrow);
+	string username;
+	string password;
+	const int maxCop = 5;
+	const int borrow = 30;
+	int books = 0;
+	bool late;
+public: 
+	Student();
+	Student(string p, string u);
+	string getUser();
+	string getPass();
+	void setPass(string p);
+	void setUser(string u);
+	void increaseB();
+	void print();
+	int getBook();
+	void setBook(int b);
+	bool getLate();
+	void setLate(bool l);
 };
