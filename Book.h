81f38fcbb5c6b55370a776f4d85d3a13374e32ef
TylerOvenden <tovenden4386@bths.edu>
2020-11-01 19:53:48 -0500

    fixxed header

--- Book.h
+++ Book.h
@@ -1,32 +1,46 @@
 #pragma once
-#pragma once
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <vector>
-#include "Book.h"
-//#include "Student.h"
+#include <list>
+#include "Student.h"
+#include <ctime>
 using namespace std;
-class Student {
+class Book {
 private:
-	string username;
-	string password;
-	const int maxCop = 5;
-	const int borrow = 30;
-	int books = 0;
-	bool late;
-public: 
-	Student();
-	Student(string p, string u);
-	string getUser();
-	string getPass();
-	void setPass(string p);
-	void setUser(string u);
-	void increaseB();
-	void print();
-	int getBook();
-	void setBook(int b);
-	bool getLate();
-	void setLate(bool l);
-        void findOverdue(Student s, vector<Book>& borrow)
+	string title;
+	string author;
+	int isbn;
+	string category;
+	int id;
+	string reader;
+	double start;
+	double expiration;
+	bool borrow;
+	bool overdue;
+
+public:
+	Book();
+	Book(string title, string author, int isbn, string category, int id);
+	void setTitle(string t);
+	string getTitle();
+	void setAuthor(string aut);
+	string getAuthor();
+	void setISBN(int is);
+	int getISBN();
+	void setCat(string cat);
+	string getCat();
+	void setID(int ide);
+	int getID();
+	void setRead(string read);
+	string getRead();
+	void setStart();
+	double getStart();
+	void setEnd(double end);
+	double getEnd();
+	double getDiff();
+	void findOverdue(Student s, vector<Book>& borrow);
+	void addCopy(Student s, int id, vector<Book>& books, vector<Book>& borrow);
+	void removeCopy(Student s, int id, vector<Book>& books, vector<Book>& borrow);
+	void findOverdue(Student s, vector<Book>& borrow)
 };
